d530ec5bfcf0abd0becb2767003b7f3f29742c66
Mike Boyer <boyer.mike.e@gmail.com>
2017-07-17 20:25:09 -0400

    Added setter/getter methods for scorekeeping and reporting

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**This is built from the Maze and Musuem projects
+ * This is designed to track each speed, frequency, and
+ * score info.
+ *  
+ * 17 JUL 2017 - Mike Boyer
+ **/
+
+public class Counter : MonoBehaviour
+{
+    private float speed;
+    private float freq;
+    private int objCount;
+    private int score;
+    private int tally;
+    
+    //Merry-Go-Round Speed
+    public float GetSpeed()
+    {
+        return speed;
+    }
+    public void SetSpeed()
+    {
+        speed = GameObject.Find("GreenLever").GetComponent<Transform>().position.z*1.12f;
+    }
+    //Bat frequency
+    public float GetFreq()
+    {
+        return freq;
+    }public void SetFreq()
+    {
+        freq = GameObject.Find("BlueLever").GetComponent<Transform>().position.z * 1.12f;
+    }
+    //Count of total objects added by player
+    public int GetObjCount()
+    {
+        return objCount;
+    }
+    public void SetObjCount(int count)
+    {
+        objCount = objCount + count;
+    }
+    //Count of totaltimes player has scored
+    public int GetScore()
+    {
+        return score;
+    }
+    public void SetScore(int win)
+    {
+        score = score + win;
+    }
+    //Count of Collectibles captured
+    public int GetTally()
+    {
+        return tally;
+    }
+    public void setTally(int newObj)
+    {
+        tally = tally + newObj;
+    }
+
+
+    
+}
+  
---
+++ Assets/Scripts/SignPostText.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+//Repurposed from my Night at the Museum and Maze Projects
+
+public class SignPostText : MonoBehaviour
+{
+    Text scoreMsg;
+
+
+    void Update()
+    {
+        float rotateSpeed = GameObject.Find("MasterLogic").GetComponent<Counter>().GetSpeed();
+        float batFreq = GameObject.Find("MasterLogic").GetComponent<Counter>().GetFreq();
+        int objCounter = GameObject.Find("MasterLogic").GetComponent<Counter>().GetObjCount();
+        int scoreCount = GameObject.Find("MasterLogic").GetComponent<Counter>().GetScore();
+        scoreMsg = GetComponent<Text>();
+        scoreMsg.text = "Merry-Go-Round Speed: " + rotateSpeed + "%\nBat Frequency: " + batFreq + "%\nYou've placed " + 
+            objCounter + " objects\nin the scene.\nYouv'e scored " + scoreCount + " times";
+
+    }
+
+    public void OnSignClick()
+    {
+        // Reset the scene when the user clicks the sign post
+        //Application.Quit();
+    }
+}
