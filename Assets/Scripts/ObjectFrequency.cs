c15fcbe8d3a0ee337ba0326b0a17715853e6b8e9
Mike Boyer <boyer.mike.e@gmail.com>
2017-07-18 20:13:44 -0400

    Variable Speed and Frequency
    Implemented a change in animation speed/freq for the merrygoround/bat based on the position of the levers in game.

---
+++ Assets/Scripts/ObjectFrequency.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFrequency : MonoBehaviour {
+
+    public Animator animObj;
+    public int freq;
+    public float animFreq;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        freq = GameObject.Find("MasterLogic").GetComponent<Counter>().GetFreq();
+        animFreq = (float)freq/30;
+        animObj.speed = animFreq;
+    }
+}
---
+++ Assets/Scripts/RotationSpeed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationSpeed : MonoBehaviour {
+
+
+    public Transform rotatingObject;
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        speed = GameObject.Find("MasterLogic").GetComponent<Counter>().GetSpeed();
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
